360b84ea3bba039e38a925beb97fd48dfae37d35
Hossara <hoseinaraghi84@gmail.com>
2022-11-09 08:24:09 +0330

    Init

---
+++ app1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write("Number : ");
+                int number = int.Parse(Console.ReadLine());
+                int mull = 1;
+
+                for (int j = 1; j <= number; j++) mull *= j;
+
+                Console.WriteLine("fc : {0}", mull);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
